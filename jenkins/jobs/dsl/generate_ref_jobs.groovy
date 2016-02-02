61b7e0c2e90018698a4b03e37e89dbf33c9824d2
Nebot, Maksym <maksym.nebot@accenture.com>
2016-02-02 12:44:19 +0200

    STABLE

--- jenkins/jobs/dsl/generate_ref_jobs.groovy
+++ jenkins/jobs/dsl/generate_ref_jobs.groovy
@@ -150,22 +150,6 @@ codeAnalysisJob.with {
         }
     }
     publishers {
-        jshint('**/jshintoutput.xml') {
-            healthLimits(3, 20)
-            thresholdLimit('high')
-            defaultEncoding('UTF-8')
-            canRunOnFailed(true)
-            useStableBuildAsReference(true)
-            useDeltaValues(true)
-            computeNew(true)
-            shouldDetectModules(true)
-            thresholds(
-                    unstableTotal: [all: 1, high: 2, normal: 3, low: 4],
-                    failedTotal: [all: 5, high: 6, normal: 7, low: 8],
-                    unstableNew: [all: 9, high: 10, normal: 11, low: 12],
-                    failedNew: [all: 13, high: 14, normal: 15, low: 16]
-            )
-        }
         downstreamParameterized {
             trigger(projectFolderName + "/deploy-nodeCIenv") {
                 condition("UNSTABLE_OR_BETTER")
