bef4e91afd7ec67d9e86431fc5378b8b299fb452
Katrina Zvaigzne <katrina.zvaigzne@accenture.com>
2016-02-03 17:08:38 +0200

    Changed name and position for security test job

--- jenkins/jobs/dsl/generate_ref_jobs.groovy
+++ jenkins/jobs/dsl/generate_ref_jobs.groovy
@@ -3,7 +3,7 @@ def workspaceFolderName = "${WORKSPACE_NAME}"
 def projectFolderName = "${PROJECT_NAME}"
 
 // Variables
-def nodeReferenceAppGitUrl = "ssh://jenkins@gerrit.service.adop.consul:29418/${PROJECT_NAME}/aowp-reference-application.git";
+def nodeReferenceAppGitUrl = "ssh://jenkins@gerrit.service.adop.consul:29418/${PROJECT_NAME}/joshua-gentess-digital-globe.git"
 
 // Jobs
 def codeAnalysisJob = freeStyleJob(projectFolderName + "/codeanalysis-nodeapp")
@@ -12,8 +12,8 @@ def deployCIJob = freeStyleJob(projectFolderName + "/deploy-nodeCIenv")
 def deployPRODNodeAJob = freeStyleJob(projectFolderName + "/deploy-PROD-node_A")
 def deployPRODNodeBJob = freeStyleJob(projectFolderName + "/deploy-PROD-node_B")
 def functionalTestAppJob = freeStyleJob(projectFolderName + "/funtionaltest-nodeapp")
+def securityTestAppJob = freeStyleJob(projectFolderName + "/securitytest-nodeapp")
 def technicalTestAppJob = freeStyleJob(projectFolderName + "/technicaltest-nodeapp")
-def automationTestAppJob = freeStyleJob(projectFolderName + "/automationtest-nodeapp")
 
 // Views
 def pipelineView = buildPipelineView(projectFolderName + "/NodejsReferenceApplication")
@@ -31,6 +31,8 @@ buildAppJob.with {
     description("Build nodejs reference app")
     wrappers {
         preBuildCleanup()
+        colorizeOutput(colorMap = 'xterm')
+        nodejs('ADOP NodeJS')
     }
     scm {
         git {
@@ -38,45 +40,56 @@ buildAppJob.with {
                 url(nodeReferenceAppGitUrl)
                 credentials("adop-jenkins-master")
             }
-            branch("*/develop")
+            branch("*/master")
         }
     }
     steps {
-        shell('''set +x
-                |if [ ! -f "${JENKINS_HOME}/tools/docker" ]; then
-                |    DOCKER_VERSION=1.6.0
-                |    mkdir -p ${JENKINS_HOME}/tools
-                |    wget https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION} --quiet -O "${JENKINS_HOME}/tools/docker"
-                |    chmod +x "${JENKINS_HOME}/tools/docker"
-                |fi
-                |
-                |project_name=$(echo ${PROJECT_NAME} | tr '[:upper:]' '[:lower:]' | tr '//' '-')
-                |${JENKINS_HOME}/tools/docker login -u devops.training -p ztNsaJPyrSyrPdtn -e devops.training@accenture.com docker.accenture.com
-                |
-                |COUNT=1
-                |while ! ${JENKINS_HOME}/tools/docker build -t docker.accenture.com/aowp/${project_name}:${BUILD_NUMBER} .
-                |do
-                |  if [ ${COUNT} -gt 10 ]; then
-                |      echo "Docker build failed even after ${COUNT}. Please investigate."
-                |      exit 1
-                |    fi
-                |    echo "Docker build failed. Retrying ..Attempt (${COUNT})"
-                |  COUNT=$((COUNT+1))
-                |done
-                |
-                |COUNT=1
-                |while ! ${JENKINS_HOME}/tools/docker push docker.accenture.com/aowp/${project_name}:${BUILD_NUMBER}
-                |do
-                |  if [ ${COUNT} -gt 10 ]; then
-                |      echo "Docker push failed even after ${COUNT}. Please investigate."
-                |      exit 1
-                |    fi
-                |    echo "Docker push failed. Retrying ..Attempt (${COUNT})"
-                |  COUNT=$((COUNT+1))
-                |done
-                |
-                |'''.stripMargin())
+        shell('''
+            |mkdir ${WORKSPACE}/bin
+            |cd ${WORKSPACE}/bin
+            |wget https://adop-framework-aowp.s3.amazonaws.com/data/software/bin/phantomjs
+            |wget https://get.docker.com/builds/Linux/x86_64/docker-latest -O ${JENKINS_HOME}/tools/docker
+            |chmod +x ${WORKSPACE}/bin/phantomjs
+            |chmod +x ${JENKINS_HOME}/tools/docker
+            |export PATH="$PATH:${WORKSPACE}/bin/"
+            '''.stripMargin())
     }
+    steps {
+        shell('''set +x
+            |if [ ! -f "${JENKINS_HOME}/tools/docker" ]; then
+            |    DOCKER_VERSION=1.9.1
+            |    mkdir -p ${JENKINS_HOME}/tools
+            |    wget https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION} --quiet -O "${JENKINS_HOME}/tools/docker"
+            |    chmod +x "${JENKINS_HOME}/tools/docker"
+            |fi
+            |project_name=$(echo ${PROJECT_NAME} | tr '[:upper:]' '[:lower:]')
+            |${JENKINS_HOME}/tools/docker login -u devops.training -p ztNsaJPyrSyrPdtn -e devops.training@accenture.com docker.accenture.com
+            |
+            |COUNT=1
+            |while ! ${JENKINS_HOME}/tools/docker build -t ${DOCKER_REGISTRY}/avs/${project_name}:${B} .
+            |do
+            |  if [ ${COUNT} -gt 10 ]; then
+            |      echo "Docker build failed even after ${COUNT}. Please investigate."
+            |      exit 1
+            |    fi
+            |    echo "Docker build failed. Retrying ..Attempt (${COUNT})"
+            |  COUNT=$((COUNT+1))
+            |done
+            |
+            |COUNT=1
+            |while ! ${JENKINS_HOME}/tools/docker push ${DOCKER_REGISTRY}/avs/${project_name}:${B}
+            |do
+            |  if [ ${COUNT} -gt 10 ]; then
+            |      echo "Docker push failed even after ${COUNT}. Please investigate."
+            |      exit 1
+            |    fi
+            |    echo "Docker push failed. Retrying ..Attempt (${COUNT})"
+            |  COUNT=$((COUNT+1))
+            |done
+            |
+            |'''.stripMargin())
+        }
+
     steps {
         systemGroovyCommand(readFileFromWorkspace("${JENKINS_HOME}/scriptler/scripts/pipeline_params.groovy"))
     }
@@ -93,11 +106,11 @@ buildAppJob.with {
                 gerritxml / 'gerritProjects' {
                     'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.data.GerritProject' {
                         compareType("PLAIN")
-                        pattern(projectFolderName + "/aowp-reference-application")
+                        pattern(projectFolderName + "/node-questionapp-reference-app")
                         'branches' {
                             'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.data.Branch' {
                                 compareType("PLAIN")
-                                pattern("develop")
+                                pattern("master")
                             }
                         }
                     }
@@ -107,6 +120,7 @@ buildAppJob.with {
         }
     }
     publishers {
+        archiveArtifacts("dist.zip")
         downstreamParameterized {
             trigger(projectFolderName + "/codeanalysis-nodeapp") {
                 condition("UNSTABLE_OR_BETTER")
@@ -122,6 +136,10 @@ buildAppJob.with {
 // Setup Load_Cartridge
 codeAnalysisJob.with {
     description("Code quality analysis for nodejs reference application using SonarQube.")
+    parameters {
+        stringParam("B", '', "Parent build number")
+        stringParam("PARENT_BUILD", "build-nodeapp", "Parent build name")
+    }
     environmentVariables {
         env('WORKSPACE_NAME', workspaceFolderName)
         env('PROJECT_NAME', projectFolderName)
@@ -131,36 +149,17 @@ codeAnalysisJob.with {
         colorizeOutput(colorMap = 'xterm')
         nodejs('ADOP NodeJS')
     }
-    scm {
-        git {
-            remote {
-                url(nodeReferenceAppGitUrl)
-                credentials("adop-jenkins-master")
+    steps {
+        copyArtifacts('build-nodeapp') {
+            buildSelector {
+                buildNumber('${B}')
             }
-            branch("*/develop")
         }
     }
-    triggers {
-        gerrit {
-            events {
-                refUpdated()
-            }
-            configure { gerritxml ->
-                gerritxml / 'gerritProjects' {
-                    'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.data.GerritProject' {
-                        compareType("PLAIN")
-                        pattern(projectFolderName + "/aowp-reference-application")
-                        'branches' {
-                            'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.data.Branch' {
-                                compareType("PLAIN")
-                                pattern("develop")
-                            }
-                        }
-                    }
-                }
-                gerritxml / serverName("ADOP Gerrit")
-            }
-        }
+    steps{
+        shell('''
+            |unzip dist.zip
+    '''.stripMargin())
     }
     configure { myProject ->
         myProject / builders << 'hudson.plugins.sonar.SonarRunnerBuilder'(plugin: "sonar@2.2.1") {
@@ -168,7 +167,7 @@ codeAnalysisJob.with {
             |sonar.projectName=${WORKSPACE_NAME}
             |sonar.projectVersion=0.0.1
             |sonar.language=js
-            |sonar.sources=app/scripts
+            |sonar.sources=scripts
             |sonar.scm.enabled=false'''.stripMargin())
             javaOpts()
             jdk('(Inherit From Job)')
@@ -239,6 +238,97 @@ functionalTestAppJob.with {
     }
     publishers {
         downstreamParameterized {
+            trigger(projectFolderName + "/securitytest-nodeapp") {
+                condition("SUCCESS")
+                parameters {
+                    predefinedProp("B", '${BUILD_NUMBER}')
+                    predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+                }
+            }
+        }
+    }
+}
+
+securityTestAppJob.with{
+    description("Tests nodejs reference app with OWASP ZAP")
+    scm{
+        git{
+            remote{
+                url(nodeReferenceAppGitUrl)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+            createTag()
+        }
+    }
+    wrappers{
+        preBuildCleanup()
+    }
+    steps{
+        shell('''echo "Running automation tests"
+                |
+                |ref=$( echo ${JOB_NAME} | sed 's#[ /]#_#g' )
+                |owasp_zap_container=owasp_zap-${ref}
+                |
+                |if [ "${JENKINS_HOME}"/tools/docker top "$owasp_zap_container" &> /dev/null ]
+                |then
+                |    ${JENKINS_HOME}/tools/docker stop $owasp_zap_container
+                |    ${JENKINS_HOME}/tools/docker rm $owasp_zap_container
+                |fi
+                |
+                |echo "Starting OWASP ZAP Intercepting Proxy"
+                |nohup ${JENKINS_HOME}/tools/docker run -i -v ${WORKSPACE}/owasp_zap_proxy/test-results/:/opt/zaproxy/test-results/ --name ${owasp_zap_container} -P docker.accenture.com/dcsc/owasp_zap_proxy /etc/init.d/zaproxy start test-${BUILD_NUMBER} &
+                |
+                |echo "Running Selenium tests through maven."
+                |sleep 30s
+                |
+                |# Setting up variables for Maven
+                |environment_ip=$(dig +short aowp-ci.node.adop.consul)
+                |node_host_id=$(echo "${NODE_NAME}" | cut -d'-' -f1 | rev | cut -d'_' -f1 | rev)
+                |
+                |VAR_APPLICATION_URL=http://${environment_ip}:80/nodeapp-ci
+                |VAR_ZAP_IP=$(dig +short jenkins-${node_host_id}.node.adop.consul)
+                |VAR_ZAP_PORT="9090"
+                |VAR_ZAP_PORT=$(${JENKINS_HOME}/tools/docker port ${owasp_zap_container} | grep "9090" | sed -rn 's#9090/tcp -> 0.0.0.0:([[:digit:]]+)$#\\1#p')
+                |
+                |echo "VAR_APPLICATION_URL=${VAR_APPLICATION_URL}" >> maven_variables.properties
+                |echo "VAR_ZAP_IP=${VAR_ZAP_IP}" >> maven_variables.properties
+                |echo "VAR_ZAP_PORT=${VAR_ZAP_PORT}" >> maven_variables.properties'''.stripMargin())
+        environmentVariables {
+            propertiesFile("maven_variables.properties")
+        }
+        maven{
+            goals("clean")
+            goals('install -B -P selenium-tests -DapplicationURL=${VAR_APPLICATION_URL} -DzapIp=${VAR_ZAP_IP} -DzapPort=${VAR_ZAP_PORT}')
+            mavenInstallation("ADOP Maven")
+        }
+        shell('''echo "Stopping OWASP ZAP Proxy and generating report."
+                |ref=$( echo ${JOB_NAME} | sed 's#[ /]#_#g' )
+                |owasp_zap_container=owasp_zap_nodeapp-${ref}
+                |
+                |${JENKINS_HOME}/tools/docker stop ${owasp_zap_container}
+                |${JENKINS_HOME}/tools/docker rm ${owasp_zap_container}
+                |
+                |${JENKINS_HOME}/tools/docker run -i -v ${WORKSPACE}/owasp_zap_proxy/test-results/:/opt/zaproxy/test-results/ docker.accenture.com/dcsc/owasp_zap_proxy /etc/init.d/zaproxy stop test-${BUILD_NUMBER}
+                |
+                |cp ${WORKSPACE}/owasp_zap_proxy/test-results/test-${BUILD_NUMBER}-report.html .'''.stripMargin())
+    }
+    publishers{
+        archiveArtifacts("*.html")
+    }
+    configure{myProject ->
+        myProject / 'publishers' / 'htmlpublisher.HtmlPublisher'(plugin:'htmlpublisher@1.4') / 'reportTargets' / 'htmlpublisher.HtmlPublisherTarget' {
+            reportName("HTML Report")
+            reportDir("nodeapp-a/target/failsafe-reports")
+            reportFiles("index.html")
+            alwaysLinkToLastBuild("false")
+            keepAll("false")
+            allowMissing("false")
+            wrapperName("htmlpublisher-wrapper.html")
+        }
+    }
+    publishers{
+        downstreamParameterized {
             trigger(projectFolderName + "/technicaltest-nodeapp") {
                 condition("SUCCESS")
                 parameters {
@@ -268,7 +358,7 @@ technicalTestAppJob.with {
         }
     }
     steps {
-        shell('''sed -i "s/http:\\/\\/nodeapp\\..*\\.xip.io/http:\\/\\/nodeapp\\.${STACK_IP}\\.xip.io/g"; ${WORKSPACE}/src/test/scala/default/RecordedSimulation.scala
+        shell('''sed -i "s/http:\\/\\/nodeapp\\..*\\.xip.io/http:\\/\\/nodeapp\\.${STACK_IP}\\.xip.io/g" ${WORKSPACE}/src/test/scala/default/RecordedSimulation.scala
     echo "$STACK_IP"'''.stripMargin())
     }
     steps {
@@ -280,87 +370,11 @@ technicalTestAppJob.with {
         downstreamParameterized {
             trigger(projectFolderName + "/deploy-PROD-node_A") {
                 condition("SUCCESS")
+                parameters {
+                    predefinedProp("B", '${BUILD_NUMBER}')
+                    predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+                }
             }
-        }
-    }
-}
-
-automationTestAppJob.with{
-    description("Tests nodejs reference app with OWASP ZAP")
-    scm{
-        git{
-            remote{
-                url(nodeReferenceAppGitUrl)
-                credentials("adop-jenkins-master")
-            }
-            branch("*/master")
-        }
-    }
-    wrappers{
-        preBuildCleanup()
-    }
-    steps{
-        shell('''echo "Running automation tests"
-                |
-                |ref=$( echo ${JOB_NAME} | sed 's#[ /]#_#g' )
-                |owasp_zap_container=owasp_zap_nodeapp-${ref}
-                |
-                |if $(${JENKINS_HOME}/tools/docker top $owasp_zap_container &> /dev/null); then
-                |    ${JENKINS_HOME}/tools/docker stop $owasp_zap_container
-                |    ${JENKINS_HOME}/tools/docker rm $owasp_zap_container
-                |fi
-                |
-                |echo "Starting OWASP ZAP Intercepting Proxy"
-                |nohup ${JENKINS_HOME}/tools/docker run -i -v ${WORKSPACE}/owasp_zap_proxy/test-results/:/opt/zaproxy/test-results/ --name ${owasp_zap_container} -P docker.accenture.com/dcsc/owasp_zap_proxy /etc/init.d/zaproxy start test-${BUILD_NUMBER} &
-                |
-                |echo "Running Selenium tests through maven."
-                |sleep 30s
-                |
-                |# Setting up variables for Maven
-                |environment_ip=$(dig +short aowp-ci.node.adop.consul)
-                |node_host_id=$(echo "${NODE_NAME}" | cut -d'-' -f1 | rev | cut -d'_' -f1 | rev)
-                |
-                |VAR_APPLICATION_URL=http://${environment_ip}:80/nodeapp-ci
-                |VAR_ZAP_IP=$(dig +short jenkins-${node_host_id}.node.adop.consul)
-                |VAR_ZAP_PORT="9090"
-                |VAR_ZAP_PORT=$(${JENKINS_HOME}/tools/docker port ${owasp_zap_container} | grep "9090" | sed -rn 's#9090/tcp -> 0.0.0.0:([[:digit:]]+)$#\\1#p')
-                |
-                |echo "VAR_APPLICATION_URL=${VAR_APPLICATION_URL}" >> maven_variables.properties
-                |echo "VAR_ZAP_IP=${VAR_ZAP_IP}" >> maven_variables.properties
-                |echo "VAR_ZAP_PORT=${VAR_ZAP_PORT}" >> maven_variables.properties'''.stripMargin())
-    }
-    environmentVariables{
-        propertiesFile('maven_variables.properties')
-    }
-    steps{
-        maven{
-              goals("clean")
-              goals('install -B -P selenium-tests -DapplicationURL=${VAR_APPLICATION_URL} -DzapIp=${VAR_ZAP_IP} -DzapPort=${VAR_ZAP_PORT}')
-              mavenInstallation("ADOP Maven")
-        }
-        shell('''echo "Stopping OWASP ZAP Proxy and generating report."
-                |ref=$( echo ${JOB_NAME} | sed 's#[ /]#_#g' )
-                |owasp_zap_container=owasp_zap_nodeapp-${ref}
-                |
-                |${JENKINS_HOME}/tools/docker stop ${owasp_zap_container}
-                |${JENKINS_HOME}/tools/docker rm ${owasp_zap_container}
-                |
-                |${JENKINS_HOME}/tools/docker run -i -v ${WORKSPACE}/owasp_zap_proxy/test-results/:/opt/zaproxy/test-results/ docker.accenture.com/dcsc/owasp_zap_proxy /etc/init.d/zaproxy stop test-${BUILD_NUMBER}
-                |
-                |cp ${WORKSPACE}/owasp_zap_proxy/test-results/test-${BUILD_NUMBER}-report.html .'''.stripMargin())
-    }
-    publishers{
-        archiveArtifacts("*.html")
-    }
-    configure{myProject ->
-        myProject / 'publishers' / 'htmlpublisher.HtmlPublisher'(plugin:'htmlpublisher@1.4') / 'reportTargets' / 'htmlpublisher.HtmlPublisherTarget' {
-            reportName("HTML Report")
-            reportDir("nodeapp-a/target/failsafe-reports")
-            reportFiles("index.html")
-            alwaysLinkToLastBuild("false")
-            keepAll("false")
-            allowMissing("false")
-            wrapperName("htmlpublisher-wrapper.html")
         }
     }
 }
