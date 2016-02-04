902708c29fbbd0b1974ee78a05d79ff08e3392fb
Anton Kaspiarovich <anton.kaspiarovich@accenture.com>
2016-02-04 18:38:25 +0200

    Sync with Gerrit

--- jenkins/jobs/dsl/generate_ref_jobs.groovy
+++ jenkins/jobs/dsl/generate_ref_jobs.groovy
@@ -5,7 +5,7 @@ def sonarProjectKey = projectFolderName.toLowerCase().replace("/", "-");
 
 // Variables
 def nodeReferenceAppGitUrl = "ssh://jenkins@gerrit.service.adop.consul:29418/${PROJECT_NAME}/aowp-reference-application.git";
-def gatelingReferenceAppGitUrl = "ssh://jenkins@gerrit.service.adop.consul:29418/${PROJECT_NAME}/gatling-tests.git";
+def gatelingReferenceAppGitUrl = "ssh://jenkins@gerrit.service.adop.consul:29418/${PROJECT_NAME}/aowp-performance-tests.git";
 
 // Jobs
 def buildAppJob = freeStyleJob(projectFolderName + "/Build_App")
@@ -56,7 +56,7 @@ buildAppJob.with {
             runner('Fail')
             steps {
                 shell('''set +x
-                        |DOCKER_VERSION=1.6.0
+                        |DOCKER_VERSION=1.7.1
                         |mkdir -p ${JENKINS_HOME}/tools
                         |wget https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION} --quiet -O "${JENKINS_HOME}/tools/docker"
                         |chmod +x "${JENKINS_HOME}/tools/docker"
@@ -119,7 +119,7 @@ buildAppJob.with {
         }
     }
     publishers {
-        archiveArtifacts("app/scripts/**/*")
+        archiveArtifacts("app/scripts/**/*, docker-compose*.yml")
         downstreamParameterized {
             trigger(projectFolderName + "/Code_Analysis") {
                 condition("UNSTABLE_OR_BETTER")
@@ -134,6 +134,10 @@ buildAppJob.with {
 
 codeAnalysisJob.with {
     description("Code quality analysis for nodejs reference application using SonarQube.")
+    parameters{
+        stringParam("B",'',"Parent build number")
+        stringParam("PARENT_BUILD",'',"Parent build name")
+    }
     environmentVariables {
         env('WORKSPACE_NAME', workspaceFolderName)
         env('PROJECT_NAME', projectFolderName)
@@ -178,14 +182,44 @@ codeAnalysisJob.with {
 
 deployToCIEnvJob.with {
     description("Deploy CI Environment Job")
+    parameters{
+        stringParam("B",'',"Parent build number")
+        stringParam("PARENT_BUILD",'',"Parent build name")
+    }
+    environmentVariables {
+        env('WORKSPACE_NAME', workspaceFolderName)
+        env('PROJECT_NAME', projectFolderName)
+        groovy("matcher = JENKINS_URL =~ /http:\\/\\/(.*?)\\/jenkins.*/; def map = [STACK_IP: matcher[0][1]]; return map;")
+    }
     wrappers {
         preBuildCleanup()
+        injectPasswords()
+        maskPasswords()
+        sshAgent("adop-jenkins-master")
+    }
+    steps {
+        copyArtifacts(projectFolderName + "/Build_App") {
+            includePatterns('docker-compose*.yml')
+            buildSelector {
+                buildNumber('${B}')
+            }
+        }
     }
     steps {
         shell('''set +x
-                |sleep 12
+                |CI_HOST="aowp-ci.node.consul"
+                |project_name=$(echo ${PROJECT_NAME} | tr '[:upper:]' '[:lower:]' | tr '//' '-')
+                |
+                |# Copy the docker-compose configuration file on CI host
+                |scp -o StrictHostKeyChecking=no docker-compose.deploy.yml ec2-user@${CI_HOST}:~/docker-compose.yml
+                |
+                |# Run docker-compose.test.yml on CI host
+                |ssh -o StrictHostKeyChecking=no ec2-user@${CI_HOST} "export project_name=${project_name}; export B=${B}; docker login -u devops.training -p ztNsaJPyrSyrPdtn -e devops.training@accenture.com docker.accenture.com; docker-compose up -d --force-recreate"
+                |
                 |echo "Deploy to CI environment completed"
-                '''.stripMargin())
+                |echo "http://${project_name}-ci.${STACK_IP}.xip.io"
+                |
+                |set -x'''.stripMargin())
     }
     publishers {
         downstreamParameterized {
@@ -202,26 +236,41 @@ deployToCIEnvJob.with {
 
 functionalTestsJob.with {
     description("Run functional tests for nodejs reference app")
+    parameters{
+        stringParam("B",'',"Parent build number")
+        stringParam("PARENT_BUILD",'',"Parent build name")
+    }
+    environmentVariables {
+        env('WORKSPACE_NAME', workspaceFolderName)
+        env('PROJECT_NAME', projectFolderName)
+    }
     wrappers {
         preBuildCleanup()
+        injectPasswords()
+        maskPasswords()
+        sshAgent("adop-jenkins-master")
     }
-    scm {
-        git {
-            remote {
-                url(nodeReferenceAppGitUrl)
-                credentials("adop-jenkins-master")
+    steps {
+        copyArtifacts(projectFolderName + "/Build_App") {
+            includePatterns('docker-compose*.yml')
+            buildSelector {
+                buildNumber('${B}')
             }
-            branch("*/master")
         }
     }
     steps {
         shell('''set +x
-                |git config --global url."https://".insteadOf git://
-                |echo $PATH
-                |npm cache clean
-                |npm install -g grunt grunt-cli --save-dev
-                |grunt test || exit 0
-                '''.stripMargin())
+                |CI_HOST="aowp-ci.node.consul"
+                |project_name=$(echo ${PROJECT_NAME} | tr '[:upper:]' '[:lower:]' | tr '//' '-')
+                |
+                |# Copy the docker-compose configuration file on CI host
+                |scp -o StrictHostKeyChecking=no docker-compose.test.yml ec2-user@${CI_HOST}:~/docker-compose.test.yml
+                |
+                |# Run docker-compose.test.yml on CI host
+                |ssh -o StrictHostKeyChecking=no ec2-user@${CI_HOST} "export project_name=${project_name}; export B=${B}; docker login -u devops.training -p ztNsaJPyrSyrPdtn -e devops.training@accenture.com docker.accenture.com; docker-compose -f docker-compose.test.yml up --force-recreate"
+                |
+                |echo "Functional tests completed."
+                |set -x'''.stripMargin())
     }
     publishers {
         downstreamParameterized {
@@ -238,6 +287,10 @@ functionalTestsJob.with {
 
 securityTestsJob.with{
     description("Tests nodejs reference app with OWASP ZAP")
+    parameters{
+        stringParam("B",'',"Parent build number")
+        stringParam("PARENT_BUILD",'',"Parent build name")
+    }
     scm {
         git {
             remote {
@@ -251,6 +304,30 @@ securityTestsJob.with{
         preBuildCleanup()
     }
     steps {
+        conditionalSteps{
+            condition{
+                shell('test ! -f "${JENKINS_HOME}/tools/.aws/bin/aws"')
+            }
+            runner("Fail")
+            steps{
+                shell('''set +x
+                        |wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip --quiet -O "${JENKINS_HOME}/tools/awscli-bundle.zip"
+                        |cd ${JENKINS_HOME}/tools && unzip -q awscli-bundle.zip
+                        |${JENKINS_HOME}/tools/awscli-bundle/install -i ${JENKINS_HOME}/tools/.aws
+                        |rm -rf ${JENKINS_HOME}/tools/awscli-bundle ${JENKINS_HOME}/tools/awscli-bundle.zip
+                        |set -x
+                        '''.stripMargin())
+            }
+            condition{
+                shell('test ! -f "${JENKINS_HOME}/tools/jq"')
+            }
+            runner('Fail')
+            steps{
+                shell('''wget -q https://s3-eu-west-1.amazonaws.com/adop-core/data-deployment/bin/jq-1.4 -O "${JENKINS_HOME}/tools/jq"
+                        |chmod +x "${JENKINS_HOME}/tools/jq"
+                        '''.stripMargin())
+            }
+        }
         shell('''echo "Running automation tests"
                 |
                 |ref=$( echo ${JOB_NAME} | sed 's#[ /]#_#g' )
@@ -269,11 +346,11 @@ securityTestsJob.with{
                 |sleep 30s
                 |
                 |# Setting up variables for Maven
-                |environment_ip=$(dig +short aowp-ci.node.adop.consul)
+                |environment_ip=$(${JENKINS_HOME}/tools/.aws/bin/aws cloudformation describe-stacks --query 'Stacks[?contains(StackName,`NodeJSEnvCartridge-Ref`)].Outputs[*]' | ${JENKINS_HOME}/tools/jq -r '.[]|.[]| select(.OutputKey=="NodeAppCIPrivateIp")|.OutputValue')
                 |node_host_id=$(echo "${NODE_NAME}" | cut -d'-' -f1 | rev | cut -d'_' -f1 | rev)
                 |
-                |VAR_APPLICATION_URL=http://${environment_ip}:80/nodeapp-ci
-                |VAR_ZAP_IP=$(dig +short jenkins-${node_host_id}.node.adop.consul)
+                |VAR_APPLICATION_URL=http://${environment_ip}:80/nodejsenvcartridge-ref-ci
+                |VAR_ZAP_IP=$(${JENKINS_HOME}/tools/.aws/bin/aws cloudformation describe-stacks --query 'Stacks[?contains(StackName,`ADOP-CORE`)].Outputs[*]' | ${JENKINS_HOME}/tools/jq -r '.[]|.[]| select(.OutputKey=="SonarJenkinsPrivateIP")|.OutputValue')
                 |VAR_ZAP_PORT="9090"
                 |VAR_ZAP_PORT=$(${JENKINS_HOME}/tools/docker port ${owasp_zap_container} | grep "9090" | sed -rn 's#9090/tcp -> 0.0.0.0:([[:digit:]]+)$#\\1#p')
                 |
@@ -330,10 +407,16 @@ securityTestsJob.with{
 
 performanceTestsJob.with {
     description("Run technical tests fot nodejs reference app")
+    parameters{
+        stringParam("B",'',"Parent build number")
+        stringParam("PARENT_BUILD",'',"Parent build name")
+    }
     wrappers {
         preBuildCleanup()
     }
     environmentVariables {
+        env('WORKSPACE_NAME', workspaceFolderName)
+        env('PROJECT_NAME', projectFolderName)
         groovy("matcher = JENKINS_URL =~ /http:\\/\\/(.*?)\\/jenkins.*/; def map = [STACK_IP: matcher[0][1]]; return map;")
     }
     scm {
@@ -346,14 +429,13 @@ performanceTestsJob.with {
         }
     }
     steps {
-        shell('''sed -i "s/http:\\/\\/nodeapp\\..*\\.xip.io/http:\\/\\/nodeapp\\.${STACK_IP}\\.xip.io/g" \${WORKSPACE}/src/test/scala/default/RecordedSimulation.scala
+        shell('''project_name=$(echo ${PROJECT_NAME} | tr '[:upper:]' '[:lower:]' | tr '//' '-')
+                |sed -i "s/http:\\/\\/nodeapp\\..*\\.xip.io/http:\\/\\/${project_name}-ci\\.${STACK_IP}\\.xip.io/g" \${WORKSPACE}/src/test/scala/default/RecordedSimulation.scala
                 |echo "$STACK_IP"
                 '''.stripMargin())
     }
     steps {
-        shell('''set +x
-                |echo "${JENKINS_URL}view/AOWP_pipeline/job/technicaltest-nodeapp/${BUILD_NUMBER}/gatling/report/recordedsimulation/source/"
-                '''.stripMargin())
+        shell('''echo "${JENKINS_URL}view/AOWP_pipeline/job/technicaltest-nodeapp/${BUILD_NUMBER}/gatling/report/recordedsimulation/source/"'''.stripMargin())
     }
     steps{
         maven {
@@ -377,59 +459,98 @@ performanceTestsJob.with {
 
 deployToProdNode1Job.with {
     description("Deploy nodejs reference app to Node A")
+    parameters{
+        stringParam("B",'',"Parent build number")
+        stringParam("PARENT_BUILD",'',"Parent build name")
+    }
+    environmentVariables {
+        env('WORKSPACE_NAME', workspaceFolderName)
+        env('PROJECT_NAME', projectFolderName)
+        groovy("matcher = JENKINS_URL =~ /http:\\/\\/(.*?)\\/jenkins.*/; def map = [STACK_IP: matcher[0][1]]; return map;")
+    }
     wrappers {
         preBuildCleanup()
+        injectPasswords()
+        maskPasswords()
+        sshAgent("adop-jenkins-master")
     }
     steps {
-        copyArtifacts('build-nodeapp') {
+        copyArtifacts(projectFolderName + "/Build_App") {
+            includePatterns('docker-compose*.yml')
             buildSelector {
                 buildNumber('${B}')
             }
         }
     }
     steps {
-        shell('''#git clone ssh://jenkins@gerrit.service.adop.consul:29418/chef_project
-                |#cp $WORKSPACE/chef_project/academy_key.pem $WORKSPACE
-                |#rm -rf chef_project
-                |#chmod 400 academy_key.pem
-                |#ssh -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt ec2-user@aowp1.service.adop.consul "/usr/bin/sudo bash -c 'chmod -R 777 /data/nodeapp; rm -rf /data/nodeapp/api/*;'"
-                |#scp -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \$WORKSPACE/api.zip ec2-user@aowp1.service.adop.consul:/data/nodeapp/api
-                |#ssh -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt ec2-user@aowp1.service.adop.consul "/usr/bin/sudo bash -c 'cd /data/nodeapp/api; unzip api.zip; rm -rf api.zip'"
-                |#ssh -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt ec2-user@aowp1.service.adop.consul "/usr/bin/sudo bash -c 'chmod -R 777 /data/nodeapp/dist; rm -rf /data/nodeapp/dist/*;'"
-                |#
-                |#scp -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \$WORKSPACE/dist.zip ec2-user@aowp1.service.adop.consul:/data/nodeapp/dist
-                |#
-                |#ssh -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt ec2-user@aowp1.service.adop.consul "/usr/bin/sudo bash -c 'cd /data/nodeapp/dist; unzip dist.zip; rm -rf dist.zip; docker restart ADOP-NodeApp-1'"
-                '''.stripMargin())
+        shell('''set +x
+                |AOWP1_HOST="aowp1.node.consul"
+                |project_name=$(echo ${PROJECT_NAME} | tr '[:upper:]' '[:lower:]' | tr '//' '-')
+                |
+                |# Copy the docker-compose configuration file on AOWP1 host
+                |scp -o StrictHostKeyChecking=no docker-compose.deploy.yml ec2-user@${AOWP1_HOST}:~/docker-compose.yml
+                |
+                |# Run docker-compose.yml on PROD1 host
+                |ssh -o StrictHostKeyChecking=no ec2-user@${AOWP1_HOST} "export project_name=${project_name}; export B=${B}; docker login -u devops.training -p ztNsaJPyrSyrPdtn -e devops.training@accenture.com docker.accenture.com; docker-compose up -d --force-recreate"
+                |
+                |echo "Deploy to PROD1 environment completed"
+                |echo "http://${project_name}-1.${STACK_IP}.xip.io"
+                |
+                |set -x'''.stripMargin())
     }
     publishers {
-        buildPipelineTrigger(projectFolderName + "/Deploy_To_Prod_Node_2")
+        downstreamParameterized {
+            trigger(projectFolderName + "/Deploy_To_Prod_Node_2") {
+                condition("SUCCESS")
+                parameters {
+                    predefinedProp("B", '${BUILD_NUMBER}')
+                    predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+                }
+            }
+        }
+
     }
 }
 
 deployToProdNode2Job.with {
     description("Deploy nodejs reference app to Node B")
+    parameters {
+        stringParam("B",'',"Parent build number")
+        stringParam("PARENT_BUILD",'',"Parent build name")
+    }
+    environmentVariables {
+        env('WORKSPACE_NAME', workspaceFolderName)
+        env('PROJECT_NAME', projectFolderName)
+        groovy("matcher = JENKINS_URL =~ /http:\\/\\/(.*?)\\/jenkins.*/; def map = [STACK_IP: matcher[0][1]]; return map;")
+    }
     wrappers {
         preBuildCleanup()
+        injectPasswords()
+        maskPasswords()
+        sshAgent("adop-jenkins-master")
     }
     steps {
-        copyArtifacts('build-nodeapp') {
+        copyArtifacts(projectFolderName + "/Build_App") {
+            includePatterns('docker-compose*.yml')
             buildSelector {
                 buildNumber('${B}')
             }
         }
     }
     steps {
-        shell('''#git clone ssh://jenkins@gerrit.service.adop.consul:29418/chef_project
-                |#cp $WORKSPACE/chef_project/academy_key.pem $WORKSPACE
-                |#rm -rf chef_project
-                |#chmod 400 academy_key.pem
-                |#ssh -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt ec2-user@aowp1.service.adop.consul "/usr/bin/sudo bash -c 'chmod -R 777 /data/nodeapp; rm -rf /data/nodeapp/api/*;'"
-                |#scp -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \$WORKSPACE/api.zip ec2-user@aowp1.service.adop.consul:/data/nodeapp/api
-                |#ssh -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt ec2-user@aowp1.service.adop.consul "/usr/bin/sudo bash -c 'cd /data/nodeapp/api; unzip api.zip; rm -rf api.zip'"
-                |#ssh -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt ec2-user@aowp1.service.adop.consul "/usr/bin/sudo bash -c 'chmod -R 777 /data/nodeapp/dist; rm -rf /data/nodeapp/dist/*;'"
-                |#scp -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \$WORKSPACE/dist.zip ec2-user@aowp1.service.adop.consul:/data/nodeapp/dist
-                |#ssh -i academy_key.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -tt ec2-user@aowp1.service.adop.consul "/usr/bin/sudo bash -c 'cd /data/nodeapp/dist; unzip dist.zip; rm -rf dist.zip; docker restart ADOP-NodeApp-1'"
-                '''.stripMargin())
+        shell('''set +x
+                |AOWP2_HOST="aowp2.node.consul"
+                |project_name=$(echo ${PROJECT_NAME} | tr '[:upper:]' '[:lower:]' | tr '//' '-')
+                |
+                |# Copy the docker-compose configuration file on AOWP2 host
+                |scp -o StrictHostKeyChecking=no docker-compose.deploy.yml ec2-user@${AOWP2_HOST}:~/docker-compose.yml
+                |
+                |# Run docker-compose.yml on PROD2 host
+                |ssh -o StrictHostKeyChecking=no ec2-user@${AOWP2_HOST} "export project_name=${project_name}; export B=${B}; docker login -u devops.training -p ztNsaJPyrSyrPdtn -e devops.training@accenture.com docker.accenture.com; docker-compose up -d --force-recreate"
+                |
+                |echo "Deploy to PROD2 environment completed"
+                |echo "http://${project_name}-2.${STACK_IP}.xip.io"
+                |
+                |set -x'''.stripMargin())
     }
 }
