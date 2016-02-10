e4373db5fdb419186a6599aeb6d0aca7717591d8
Kovalenko <arturs.kovalenko@accenture.com>
2016-02-10 14:05:23 +0200

    modified parameters

--- jenkins/jobs/dsl/environment_provisioning.groovy
+++ jenkins/jobs/dsl/environment_provisioning.groovy
@@ -4,12 +4,13 @@ def projectFolderName = "${PROJECT_NAME}"
 
 // Variables
 def environmentTemplateGitUrl = "ssh://git@uat.alm.accenture.com/ado7/cartridge-aowp-v5.git"
+def projectNameKey = projectFolderName.toLowerCase().replace("/", "-");
+def fullEnvironmentName = projectFolderName.replace("/", "-");
 
 // Jobs
 def environmentProvisioningPipelineView = buildPipelineView(projectFolderName + "/Environment_Provisioning")
 def createEnvironmentJob = freeStyleJob(projectFolderName + "/Create_Environment")
 def destroyEnvironmentJob = freeStyleJob(projectFolderName + "/Destroy_Environment")
-def createIrisFrontEndJob = freeStyleJob(projectFolderName + "/Create_Iris_Frontend")
 
 // Pipeline
 environmentProvisioningPipelineView.with{
@@ -27,6 +28,8 @@ createEnvironmentJob.with{
     environmentVariables {
         env('WORKSPACE_NAME',workspaceFolderName)
         env('PROJECT_NAME',projectFolderName)
+        env('PROJECT_NAME_KEY',projectNameKey)
+        env('FULL_ENVIRONMENT_NAME',fullEnvironmentName)
     }
     steps {
         shell('''#!/bin/bash -e
@@ -37,46 +40,45 @@ TOKEN_IP="###TOKEN_IP###"
 TOKEN_PORT="###TOKEN_PORT###"
 
 # Define required variables
-FULL_ENVIRONMENT_NAME=$( echo "${PROJECT_NAME}" | sed "s#[\\/_ ]#-#g" )
-FULL_ENVIRONMENT_NAME_LOWERCASE=$(echo ${FULL_ENVIRONMENT_NAME} | tr '[:upper:]' '[:lower:]')
-
 node_names_list=(NodeAppCI NodeApp1 NodeApp2)
 
 echo "FULL_ENVIRONMENT_NAME=$FULL_ENVIRONMENT_NAME" > endpoints.txt
 
 # Copy main NGINX config
-nginx_main_env_conf="${FULL_ENVIRONMENT_NAME_LOWERCASE}.conf"
+nginx_main_env_conf="${PROJECT_NAME_KEY}.conf"
 cp environment/nginx/nodeapp.conf ${nginx_main_env_conf}
 
 # Copy public NGINX config
-nginx_public_env_conf="${FULL_ENVIRONMENT_NAME_LOWERCASE}-public.conf"
+nginx_public_env_conf="${PROJECT_NAME_KEY}-public.conf"
 cp environment/nginx/nodeapp-public.conf ${nginx_public_env_conf}
 
 # Loop trough the node list starting containers and generating nginx configuration
 for node_name in ${node_names_list[@]}; do
   # Define all the variables
   node_name_lowercase=$(echo ${node_name} | tr '[:upper:]' '[:lower:]')
-  full_site_name="${FULL_ENVIRONMENT_NAME_LOWERCASE}-${node_name_lowercase}"
+  full_site_name="${PROJECT_NAME_KEY}-${node_name_lowercase}"
   nginx_sites_enabled_file="${full_site_name}.conf"
 
   SITE_NAME=$(echo ${node_name} | sed "s/NodeApp//g")
-  SERVICE_NAME="${FULL_ENVIRONMENT_NAME}-${node_name}"
     
   echo "${node_name}=${full_site_name}" >> endpoints.txt
     
     if [ "${SITE_NAME}" != "CI" ]
     then
-      sed -i "s/${TOKEN_NAMESPACE}/${FULL_ENVIRONMENT_NAME}/g" ${nginx_main_env_conf} ${nginx_public_env_conf}
-      sed -i "s/###TOKEN_NODEAPP_${SITE_NAME}_IP###/${SERVICE_NAME}/g" ${nginx_main_env_conf} ${nginx_public_env_conf}
-      sed -i "s/###TOKEN_NODEAPP_${SITE_NAME}_PORT###/8080/g" ${nginx_main_env_conf} ${nginx_public_env_conf}
+        SERVICE_NAME="${PROJECT_NAME_KEY}-${SITE_NAME}"
+        sed -i "s/${TOKEN_NAMESPACE}/${PROJECT_NAME_KEY}/g" ${nginx_main_env_conf} ${nginx_public_env_conf}
+        sed -i "s/###TOKEN_NODEAPP_${SITE_NAME}_IP###/${SERVICE_NAME}/g" ${nginx_main_env_conf} ${nginx_public_env_conf}
+        sed -i "s/###TOKEN_NODEAPP_${SITE_NAME}_PORT###/8080/g" ${nginx_main_env_conf} ${nginx_public_env_conf}
 
-      # Upload new config on ADOP-NGINX server
-      docker cp ${nginx_main_env_conf} proxy:/etc/nginx/sites-enabled/${nginx_main_env_conf}
-      docker cp ${nginx_public_env_conf} proxy:/etc/nginx/sites-enabled/${nginx_public_env_conf}
+        # Upload new config on ADOP-NGINX server
+        docker cp ${nginx_main_env_conf} proxy:/etc/nginx/sites-enabled/${nginx_main_env_conf}
+        docker cp ${nginx_public_env_conf} proxy:/etc/nginx/sites-enabled/${nginx_public_env_conf}
+    else
+        SERVICE_NAME="${PROJECT_NAME_KEY}-prod${SITE_NAME}"
     fi
 
     # Run the docker container for the current node
-    environment_configs_mask="${FULL_ENVIRONMENT_NAME_LOWERCASE}*"
+    environment_configs_mask="$PROJECT_NAME_KEY}*"
 
     # Genrate nginx configuration
     cp environment/nginx/nodeapp-env.conf ${nginx_sites_enabled_file}
@@ -126,6 +128,8 @@ destroyEnvironmentJob.with{
     environmentVariables {
         env('WORKSPACE_NAME',workspaceFolderName)
         env('PROJECT_NAME',projectFolderName)
+        env('PROJECT_NAME_KEY',projectNameKey)
+        env('FULL_ENVIRONMENT_NAME',fullEnvironmentName)
     }
     steps {
         shell('''#!/bin/bash -e
@@ -133,9 +137,8 @@ destroyEnvironmentJob.with{
 # Define variables
 node_names_list=(NodeAppCI NodeApp1 NodeApp2)
 
-FULL_ENVIRONMENT_NAME_LOWERCASE=$(echo ${FULL_ENVIRONMENT_NAME} | tr '[:upper:]' '[:lower:]')
-nginx_main_env_conf="${FULL_ENVIRONMENT_NAME_LOWERCASE}.conf"
-nginx_public_env_conf="${FULL_ENVIRONMENT_NAME_LOWERCASE}-public.conf"
+nginx_main_env_conf="${PROJECT_NAME_KEY}.conf"
+nginx_public_env_conf="${PROJECT_NAME_KEY}-public.conf"
 
 # Remove entry from Nginx
 echo "Deleting main Nginx configuration"
@@ -144,8 +147,7 @@ docker exec -i proxy rm /etc/nginx/sites-enabled/${nginx_public_env_conf}
 
 for node_name in ${node_names_list[@]}; do
     echo "Deleting Nginx configuation and removing Docker container for ${node_name}"
-    nginx_sites_enabled_file="${FULL_ENVIRONMENT_NAME_LOWERCASE}-$(echo ${node_name} | tr '[:upper:]' '[:lower:]').conf"
-    full_node_name="${FULL_ENVIRONMENT_NAME}-${node_name}"
+    nginx_sites_enabled_file="${PROJECT_NAME_KEY}-$(echo ${node_name} | tr '[:upper:]' '[:lower:]').conf"
     docker exec -i proxy rm /etc/nginx/sites-enabled/${nginx_sites_enabled_file}
 done
 
