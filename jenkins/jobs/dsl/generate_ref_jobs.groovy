32fa57c4e58e70659634b42b6c7ae290553b34c3
Nebot, Maksym <maksym.nebot@accenture.com>
2016-02-10 17:43:40 +0200

    Minor Fixes.

--- jenkins/jobs/dsl/generate_ref_jobs.groovy
+++ jenkins/jobs/dsl/generate_ref_jobs.groovy
@@ -30,10 +30,7 @@ generateNodeReferenceAppJobs.with {
             | echo "Creating gerrit project : ${PROJECT_NAME}/${GIT_REPOSITORY} "
             | ssh -p 29418 gerrit.service.adop.consul gerrit create-project ${PROJECT_NAME}/${GIT_REPOSITORY} --empty-commit
             | # Populate repository
-            |repo_name=$GIT_REPOSITORY
-            |target_repo_name="${repo_namespace}/${repo_name}"
-            |git clone ssh://jenkins@gerrit.service.adop.consul:29418/"${target_repo_name}"
-            |cd "${repo_name}"
+            |git clone ssh://gerrit.service.adop.consul:29418/${PROJECT_NAME}/${GIT_REPOSITORY} .
             |git remote add source "${GIT_REPOSITORY_URL}"
             |git fetch source
             |git push origin +refs/remotes/source/*:refs/heads/*
